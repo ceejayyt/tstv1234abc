269b4e3d5896dbd700070aa71e25b0df7316a960
CeeJayYT <101416503+ceejayyt@users.noreply.github.com>
2025-04-14 14:26:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,14 @@
+local cfg = {
+	menu_title = "cCheats : Elitelupus Private",
+	menu_version = "Beta 1.0.0"
+}
 
+local colors = {
+  header = Color(70, 73, 76),
+  background = Color(76, 92, 104),
+  text = Color(220, 220, 221),
+  s_text = Color(197, 195, 198),
+  accent = Color(25, 133, 161),
+  enable = Color(59, 255, 20),
+  disable = Color(255, 20, 67)
+}
